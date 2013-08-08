1c0f4619e1f67aed97c41aa4a4a51b6f13d4fce2
Ben Fox-Moore <ben.foxmoore@gmail.com>
2013-08-08 15:13:53 +0200

    Add ability to disable sound

--- led.py
+++ led.py
@@ -10,22 +10,24 @@ TURN_LIGHT_OFF = "\x21\x00\x55" #this turns all lights off
 TURN_LIGHT_ON = "\x22\x00\x55"
 BRIGHT_UP = "\x23\x00\x55"
 BRIGHT_DOWN = "\x24\x00\x55"
-MODE_DOWN = "\x28\x00\x55"
+SPEED_UP = "\x25\x00\x55"
+SPEED_DOWN = "\x26\x00\x55"
 MODE_UP = "\x27\x00\x55"
+MODE_DOWN = "\x28\x00\x55"
 GREEN = "\x20\x70\x55"
 RED = "\x20\xB0\x55"
 
-def send(sock, msg, sleep=0.5):
+def send(sock, msg, sleep=0.3):
 	sock.sendto(msg, (UDP_IP, UDP_PORT))
 	time.sleep(sleep)
 
-def demo(sock):
+def demo(sock, sound=True):
 	# Green Light
 	success_mode(sock)
 	time.sleep(5)
 
 	# Play sound
-	fail_mode(sock, 5)
+	fail_mode(sock, sound, 10)
 
 	# Switch back to Green
 	success_mode(sock)
@@ -33,26 +35,29 @@ def demo(sock):
 def success_mode(sock):
 	send(sock, GREEN)
 	for i in range(9):
-		send(sock, BRIGHT_DOWN)
+		send(sock, BRIGHT_DOWN, 0.1)
 
-def fail_mode(sock, sleep=0):
+def fail_mode(sock, soundOn=True, sleep=0):
 	# Play sound
-	pygame.init()
-	sound = pygame.mixer.Sound('sound.wav')
-	sound.set_volume(1.0)
-	sound.play()
-	time.sleep(2)
+	if soundOn:
+		pygame.init()
+		sound = pygame.mixer.Sound('sound.wav')
+		sound.set_volume(1.0)
+		sound.play()
+		time.sleep(2)
 	send(sock, GREEN)
 	# Switch to 'Mode' mode - should be set to flashing red manually first
 	send(sock, MODE_DOWN)
 	for i in range(9):
-		send(sock, BRIGHT_UP)
+		send(sock, BRIGHT_UP, 0.05)
 	if (sleep > 0):
 		time.sleep(sleep)
-		sound.stop()
+		if soundOn:
+			sound.stop()
 	else:
-		time.sleep(sound.get_length())
-		sound.stop()
+		if soundOn:
+			time.sleep(sound.get_length())
+			sound.stop()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 send(sock, TURN_LIGHT_ON)
@@ -60,16 +65,30 @@ send(sock, TURN_LIGHT_ON)
 arg_len = len(sys.argv)
 if (arg_len == 1):
 	demo(sock)
-elif (arg_len == 2):
-	success = sys.argv[1]
-	if (success == "1"):
-		success_mode(sock)
-	elif (success == "0"):
-		fail_mode(sock)
+if (arg_len == 2):
+	if (sys.argv[1] == "-ns"):
+		demo(sock, False)
 	else:
-		print "Error: argument for success/failure should either be 1 for success or 0 for failure"
+		success = sys.argv[1]
+		if (success == "1"):
+			success_mode(sock)
+		elif (success == "0"):
+			fail_mode(sock)
+		else:
+			print "Error: argument for success/failure should either be 1 for success or 0 for failure"
+elif (arg_len == 3):
+	if (sys.argv[1] != "-ns"):
+		print "Error: argument for success/failure should either be 1 for success or 0 for failure, with optional argument \"-ns\" for no sound"
+	else:
+		success = sys.argv[2]
+		if (success == "1"):
+			success_mode(sock)
+		elif (success == "0"):
+			fail_mode(sock, False)
+		else:
+			print "Error: argument for success/failure should either be 1 for success or 0 for failure"
 else:
-	print "Error: 1 argument should be passed indicating success or failure (1/0), or no arguments for demo mode"
+	print "Error: 1 argument should be passed indicating success or failure (1/0), or no arguments for demo mode. Optional argument of \"-ns\" for no sound"
 	print "Args: ", str(sys.argv)
 
 
