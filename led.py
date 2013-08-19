b37ce1b107690e6c0962dd2eb488283064f876f5
Ben Fox-Moore <ben.foxmoore@gmail.com>
2013-08-19 14:47:47 +0200

    Add server mode to listen for UDP build results
    Add logging
    Improve option parsing

--- led.py
+++ led.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python
+
 import socket
 import time
 import pygame
 import sys
+import json
+import getopt
+import logging
 
-UDP_IP = "192.168.1.15" #this is the IP of the wifi bridge
-UDP_PORT = 50000
+SEND_UDP_IP = "192.168.1.15" #this is the IP of the wifi bridge
+SEND_UDP_PORT = 50000
+
+LISTEN_UDP_IP = "0.0.0.0"
+LISTEN_UDP_PORT = 50000
 
 TURN_LIGHT_OFF = "\x21\x00\x55" #this turns all lights off
 TURN_LIGHT_ON = "\x22\x00\x55"
@@ -17,17 +25,23 @@ MODE_DOWN = "\x28\x00\x55"
 GREEN = "\x20\x70\x55"
 RED = "\x20\xB0\x55"
 
+QUIET = False
+MODE = ""
+
+LOG_LEVEL = logging.INFO
+LOG_FORMAT = "%(asctime)s | %(levelname)7s | %(message)s"
+
 def send(sock, msg, sleep=0.3):
-	sock.sendto(msg, (UDP_IP, UDP_PORT))
+	sock.sendto(msg, (SEND_UDP_IP, SEND_UDP_PORT))
 	time.sleep(sleep)
 
-def demo(sock, sound=True):
+def demo(sock):
 	# Green Light
 	success_mode(sock)
 	time.sleep(5)
 
-	# Play sound
-	fail_mode(sock, sound, 10)
+	# Show fail mode
+	fail_mode(sock, 10)
 
 	# Switch back to Green
 	success_mode(sock)
@@ -37,9 +51,9 @@ def success_mode(sock):
 	for i in range(9):
 		send(sock, BRIGHT_DOWN, 0.1)
 
-def fail_mode(sock, soundOn=True, sleep=0):
+def fail_mode(sock, sleep=0):
 	# Play sound
-	if soundOn:
+	if not QUIET:
 		pygame.init()
 		sound = pygame.mixer.Sound('sound.wav')
 		sound.set_volume(1.0)
@@ -52,44 +66,111 @@ def fail_mode(sock, soundOn=True, sleep=0):
 		send(sock, BRIGHT_UP, 0.05)
 	if (sleep > 0):
 		time.sleep(sleep)
-		if soundOn:
+		if not QUIET:
 			sound.stop()
 	else:
-		if soundOn:
+		if not QUIET:
 			time.sleep(sound.get_length())
 			sound.stop()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-send(sock, TURN_LIGHT_ON)
+def call_corresponding_mode(sock, status):
+	if (status == "1" or status == "SUCCESS"):
+		success_mode(sock)
+	elif (status == "0" or status == "FAILURE"):
+		fail_mode(sock)
+	else:
+		print "Status should either be 1/0 or SUCCESS/FAILURE"
 
-arg_len = len(sys.argv)
-if (arg_len == 1):
-	demo(sock)
-if (arg_len == 2):
-	if (sys.argv[1] == "-ns"):
-		demo(sock, False)
+def print_help(exit_code, exit=True):
+	print 'usage: led.py [-q] [--log=LEVEL] status'
+	print '       led.py [-q] [--log=LEVEL] --server'
+	print '       led.py [-q] [--log=LEVEL] --demo'
+	if exit:
+		sys.exit(exit_code)
+
+def set_mode(new_mode):
+	global MODE
+	if MODE == "":
+		MODE = new_mode
+		return True
 	else:
-		success = sys.argv[1]
-		if (success == "1"):
-			success_mode(sock)
-		elif (success == "0"):
-			fail_mode(sock)
-		else:
-			print "Error: argument for success/failure should either be 1 for success or 0 for failure"
-elif (arg_len == 3):
-	if (sys.argv[1] != "-ns"):
-		print "Error: argument for success/failure should either be 1 for success or 0 for failure, with optional argument \"-ns\" for no sound"
+		return False
+
+def parse_arguments():
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"qsdh",["quiet","server", "demo", "help", "log="])
+	except getopt.GetoptError:
+		print_help(2)
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print_help(0)
+		elif opt in ("-q", "--quiet"):
+			QUIET = True
+		elif opt in ("-s", "--server"):
+			if not set_mode("SERVER"):
+				print_help(1)
+		elif opt in ("-d", "--demo"):
+			if not set_mode("DEMO"):
+				print_help(1)
+		elif opt in ("--log"):
+			numeric_level = getattr(logging, arg.upper(), None)
+			if not isinstance(numeric_level, int):
+			    raise ValueError('Invalid log level: %s' % arg)
+			global LOG_LEVEL
+			LOG_LEVEL = numeric_level
+	set_mode("DIRECT")
+	return args
+
+args = parse_arguments()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(LOG_LEVEL)
+
+fh = logging.FileHandler('led.log')
+ch = logging.StreamHandler()
+formatter = logging.Formatter(LOG_FORMAT)
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+logger.debug("Set Log Level to " + logging.getLevelName(LOG_LEVEL))
+
+send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+send(send_sock, TURN_LIGHT_ON)
+
+if MODE == "SERVER":
+
+	listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	listen_sock.bind((LISTEN_UDP_IP, LISTEN_UDP_PORT))
+
+	while True:
+		data, addr = listen_sock.recvfrom(1024) # buffer size is 1024 bytes
+		logger.debug("Received \"" + data + "\"")
+		try:
+			info = json.loads(data)
+			build_info = info['build']
+			if build_info['phase'] == "FINISHED":
+				status = build_info['status']
+				logger.info("Build finished with status %s", status)
+				call_corresponding_mode(send_sock, status)
+		except ValueError as e:
+			logger.warning(e)
+			if LOG_LEVEL > logging.DEBUG:
+				logger.warning(data)
+		except:
+			e = sys.exc_info()[0]
+			logger.error(e)
+elif MODE == "DEMO":
+	demo(send_sock)
+elif MODE == "DIRECT":
+	arg_len = len(args)
+	if arg_len != 1:
+		print_help(1)
 	else:
-		success = sys.argv[2]
-		if (success == "1"):
-			success_mode(sock)
-		elif (success == "0"):
-			fail_mode(sock, False)
-		else:
-			print "Error: argument for success/failure should either be 1 for success or 0 for failure"
-else:
-	print "Error: 1 argument should be passed indicating success or failure (1/0), or no arguments for demo mode. Optional argument of \"-ns\" for no sound"
-	print "Args: ", str(sys.argv)
+		status = args[0]
+		call_corresponding_mode(send_sock, status)
+
 
 
 
